933735039b04c554bc46f7e986d46978b59408fb
elecbug <deveb1479@gmail.com>
2025-08-18 18:33:11 +0900

    chore: add test shell

---
+++ test.sh
@@ -0,0 +1,3 @@
+go test -v network-graph/
+python3 tester.py -i network-graph/directional.graph.log -c network-graph/directional.metrics.log -r directional.report.log -o directional.out.log
+python3 tester.py -i network-graph/bidirectional.graph.log -c network-graph/bidirectional.metrics.log -r bidirectional.report.log -o bidirectional.out.log
