b147b8b74a90d25049d3d15283b8349bbb740b72
elecbug <deveb1479@gmail.com>
2025-08-19 16:49:54 +0900

    feat: update cache clear func

--- network-graph/algorithm/algorithm.go
+++ network-graph/algorithm/algorithm.go
@@ -10,3 +10,11 @@ import (
 var cachedAllShortestPaths = make(map[string]path.GraphPaths)
 var cachedAllShortestPathLengths = make(map[string]path.PathLength)
 var cacheMu sync.RWMutex
+
+// CacheClear clears the cached shortest paths and their lengths.
+func CacheClear() {
+	cacheMu.Lock()
+	defer cacheMu.Unlock()
+	cachedAllShortestPaths = make(map[string]path.GraphPaths)
+	cachedAllShortestPathLengths = make(map[string]path.PathLength)
+}
