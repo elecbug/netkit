dac872828ead53496b416ce70d5ef132d7913273
elecbug <deveb1479@gmail.com>
2025-10-07 15:01:55 +0900

    feat: fix test

--- p2p/p2p_test.go
+++ p2p/p2p_test.go
@@ -29,8 +29,8 @@ func TestGenerateNetwork(t *testing.T) {
 	msg := "Hello, P2P World!"
 
 	p2p.RunNetworkSimulation(nw)
+	time.Sleep(1 * time.Second)
 	p2p.Publish(nw[0], msg)
-
 	time.Sleep(5 * time.Second)
 
 	count := 0
@@ -54,3 +54,51 @@ func TestGenerateNetwork(t *testing.T) {
 
 	os.WriteFile("p2p_result.log", data, 0644)
 }
+
+// func TestExpCase(t *testing.T) {
+// 	for i := 4; i < 10; i++ {
+// 		for j := 0; j < 100; j++ {
+// 			filename := fmt.Sprintf("data/p2p_result-%02d-%03d.log", i, j)
+
+// 			if _, err := os.Stat(filename); err == nil {
+// 				t.Logf("File %s already exists, skipping...\n", filename)
+// 				continue
+// 			}
+
+// 			t.Logf("Experiment case: %02d-%03d\n", i, j)
+// 			src := rand.NewSource(time.Now().UnixNano())
+// 			r := rand.New(src)
+
+// 			n := r.Int()%(128-119) + 119
+// 			g := standard_graph.ErdosRenyiGraph(n, float64(i)/float64(n), true)
+
+// 			nodeLatency := func() float64 { return p2p.LogNormalRand(5.704, 0.5, src) }
+// 			edgeLatency := func() float64 { return p2p.LogNormalRand(5.704, 0.3, src) }
+
+// 			nw, _ := p2p.GenerateNetwork(g, nodeLatency, edgeLatency)
+// 			msg := "Hello, P2P World!"
+
+// 			t.Logf("Generated graph with %d nodes and %d edges\n", len(g.Nodes()), g.EdgeCount())
+
+// 			p2p.RunNetworkSimulation(nw)
+// 			time.Sleep(1 * time.Second)
+// 			p2p.Publish(nw[0], msg)
+// 			time.Sleep(5 * time.Second)
+
+// 			result := make(map[string]map[string]any)
+
+// 			for id, node := range nw {
+// 				result[fmt.Sprintf("node_%d", id)] = map[string]any{}
+// 				result[fmt.Sprintf("node_%d", id)]["recv"] = node.RecvFrom[msg]
+// 				result[fmt.Sprintf("node_%d", id)]["sent"] = node.SentTo[msg]
+// 				result[fmt.Sprintf("node_%d", id)]["seen"] = node.SeenAt[msg]
+// 			}
+
+// 			data, _ := json.Marshal(result)
+
+// 			os.WriteFile(filename, data, 0644)
+
+// 			algorithm.CacheClear()
+// 		}
+// 	}
+// }
