7320df2f551ce70a4dbc3c6609f76c1d13ad79e5
elecbug <deveb1479@gmail.com>
2025-11-25 13:29:04 +0900

    fix: shell

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-go test -v ./network-graph/
+go test -v ./graph/
 
 cd script
 
-python3 compare_metrics.py -i ../network-graph/directional.graph.log -c ../network-graph/directional.metrics.log -r ./directional.report.log -o ./directional.out.log
-python3 compare_metrics.py -i ../network-graph/bidirectional.graph.log -c ../network-graph/bidirectional.metrics.log -r ./bidirectional.report.log -o ./bidirectional.out.log
+python3 compare_metrics.py -i ../graph/directional.graph.log -c ../graph/directional.metrics.log -r ./directional.report.log -o ./directional.out.log
+python3 compare_metrics.py -i ../graph/bidirectional.graph.log -c ../graph/bidirectional.metrics.log -r ./bidirectional.report.log -o ./bidirectional.out.log
 
 cd ..
